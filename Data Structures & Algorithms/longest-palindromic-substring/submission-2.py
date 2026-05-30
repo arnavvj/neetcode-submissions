fc81cf26dbc46b045ce83b89ec7a9c831c8a5592
Arnav Jeurkar <153469685+arnavvj@users.noreply.github.com>
2026-05-30 00:13:21 -0500

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def longestPalindrome(self, s: str) -> str:
+
+        j = 0
+        max_len = 1
+        ans = s[0]
+
+        while (j < len(s)):
+
+            # odd centered substring i.e. `cabac`
+            i, k = j-1, j+1
+            while(i >= 0 and k < len(s)):
+                if s[i] != s[k]:
+                    break
+                if k-i+1 > max_len:
+                    max_len = k-i+1
+                    ans = s[i:k+1]
+                i -= 1
+                k += 1
+                
+            # even centered substring i.e. `cbbc`
+            i, k = j, j+1 
+            while(i >= 0 and k < len(s)):
+                if s[i] != s[k]:
+                    break
+                if k-i+1 > max_len:
+                    max_len = k-i+1
+                    ans = s[i:k+1]
+                i -= 1
+                k += 1
+                        
+            j += 1
+
+        return ans
+
+
+"""
+
+Every character as center of palindrome, expand and check!!
+
+a s d f g f d s e r g r e s d m m m f
+        ^
+  ^   <-|->   ^
+
+"""
